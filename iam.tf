ba54eaf9e3a39f330f30deaff4254e27d5360445
Justin Reeves <reeves122@gmail.com>
2023-07-19 16:37:39 -0500

    create start_replication_task lambda

---
+++ iam.tf
@@ -0,0 +1,12 @@
+data "aws_iam_policy_document" "this" {
+  statement {
+    actions   = ["dms:StartReplicationTask"]
+    resources = [aws_dms_replication_task.this.replication_task_arn]
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name_prefix = var.git
+  policy      = data.aws_iam_policy_document.this.json
+  tags        = local.tags
+}
