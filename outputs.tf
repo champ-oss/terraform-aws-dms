c11345468987f157a0aa30a6805537894ee5c7e8
Justin Reeves <reeves122@gmail.com>
2023-07-27 13:21:15 -0500

    add output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,29 @@
 output "replication_task_arn" {
-  value = aws_dms_replication_task.this.replication_task_arn
+  description = "https://registry.terraform.io/providers/hashicorp/aws/3.8.0/docs/resources/dms_replication_task"
+  value       = aws_dms_replication_task.this.replication_task_arn
 }
 
 output "replication_instance_arn" {
-  value = aws_dms_replication_task.this.replication_instance_arn
+  description = "https://registry.terraform.io/providers/hashicorp/aws/3.8.0/docs/resources/dms_replication_task"
+  value       = aws_dms_replication_task.this.replication_instance_arn
 }
 
 output "source_endpoint_arn" {
-  value = aws_dms_replication_task.this.source_endpoint_arn
+  description = "https://registry.terraform.io/providers/hashicorp/aws/3.8.0/docs/resources/dms_replication_task"
+  value       = aws_dms_replication_task.this.source_endpoint_arn
 }
 
 output "migration_type" {
-  value = aws_dms_replication_task.this.migration_type
+  description = "https://registry.terraform.io/providers/hashicorp/aws/3.8.0/docs/resources/dms_replication_task"
+  value       = aws_dms_replication_task.this.migration_type
 }
 
 output "replication_task_id" {
-  value = aws_dms_replication_task.this.replication_task_id
+  description = "https://registry.terraform.io/providers/hashicorp/aws/3.8.0/docs/resources/dms_replication_task"
+  value       = aws_dms_replication_task.this.replication_task_id
 }
 
 output "target_endpoint_arn" {
-  value = aws_dms_replication_task.this.target_endpoint_arn
+  description = "https://registry.terraform.io/providers/hashicorp/aws/3.8.0/docs/resources/dms_replication_task"
+  value       = aws_dms_replication_task.this.target_endpoint_arn
 }
