93c15e802ffa49dd7c39af62cd45f3b07870c069
Justin Reeves <reeves122@gmail.com>
2023-09-26 15:20:57 -0500

    import dms-vpc-role

--- examples/complete/iam.tf
+++ examples/complete/iam.tf
@@ -24,6 +24,11 @@ resource "aws_iam_role_policy_attachment" "amazon_dms_cloudwatch_logs_role" {
   role       = aws_iam_role.dms_cloudwatch_logs_role.name
 }
 
+import {
+  to = aws_iam_role.dms_vpc_role
+  id = "dms-vpc-role"
+}
+
 resource "aws_iam_role" "dms_vpc_role" {
   assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
   name               = "dms-vpc-role"
